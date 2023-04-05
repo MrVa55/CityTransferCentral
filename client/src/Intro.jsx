ae773e28eed420bc422257860c603082b777e8d5
Thomas Vass <thomasstefanvass@gmail.com>
2023-04-04 21:58:51 -0400

    typos

--- client/src/Intro.jsx
+++ client/src/Intro.jsx
@@ -20,16 +20,16 @@ function Intro() {
       The project demonstrates the use of public key cryptography through ECDSA (Elliptic Curve Digital Signature Algorithm).
       </Text>
       <Text>  <br />    
-      In this project private keys are generated from 2 seed secret seed words and cities are used as private addresses allowing users to travel the globe.
-      It securely identifies the sender of coins without transferring the private key and protects against tampering and replay attacks. 
+      In this project private keys are generated from 2 seed secret seed words and city names are used as public addresses allowing users to travel the globe.
+      It securely identifies the sender of "gold coin" transactions without transferring the private key and protects against tampering and replay attacks. 
       </Text>
       <Text> <br />
       The team behind CityTransfer CENTRAL are aware that the number of cities in the world could be a bottleneck to adaptation, but as a school project such growth can not be anticipated in the near future.
       </Text>
       <Text><br />
-        Instead, a fun globetrotter game arises. Because all cities start with 100 gold coins and can only be claimed once, there is an incentive for users to claim new cities first and transfer the funds to their HQ.
+        Instead, using cities as public addresses gives rise to a fun globetrotter game. Because all cities start with 100 gold coins and can only be claimed once, there is an incentive for users to claim new cities first and transfer the funds to their HQ.
         </Text><br />
-      <Text>Have fun! </Text>
+      <Text>Try it out and have fun! </Text>
    
 
       
